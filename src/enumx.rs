034b5bd12fb4f2a4981df004501bd45a1f21a046
wuquan <17324432872@163.com>
2025-01-15 17:33:32 +0800

    feat  1.add match

--- src/enumx.rs
+++ src/enumx.rs
@@ -2,3 +2,26 @@ enum IpAddKind{
     V4,
     V6
 }
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) ->u8{
+    match coin {
+        Coin::Dime => 10,
+        Coin::Nickel => 5,
+        Coin::Penny => 1,
+        Coin::Quarter => 25,
+    }
+}
+
+fn plus_one(x:Option<i32>)-> Option<i32>{
+    match x {
+        None => None,
+        Some(i) => Some(i+1),
+    }
+}
